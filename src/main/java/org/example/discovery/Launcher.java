f3fd8d8b2b146c1fcadf634aab9f8d25b2a09def
steven na <theyoung2002@naver.com>
2023-02-16 15:39:18 +0900

    - archive jar test success

---
+++ src/main/java/org/example/discovery/Launcher.java
@@ -0,0 +1,15 @@
+package org.example.discovery;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.logging.Logger;
+
+public class Launcher {
+    static final Log log = LogFactory.getLog(Launcher.class);
+
+    public static void main(String[] args) {
+//        System.out.println("test");
+        log.debug("test");
+    }
+}
---
+++ src/test/java/org/example/discovery/LauncherTest.java
@@ -0,0 +1,11 @@
+package org.example.discovery;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LauncherTest {
+    @Test
+    void test_test(){
+        Assertions.assertTrue(true);
+    }
+}
